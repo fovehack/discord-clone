c0b8e60ffd204fa2a01932d48765c4c88084b75d
MRFOVE\Fove <fovehack@gmail.com>
2023-09-09 22:10:11 +0200

    final

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    webpack: (config) => {
+      config.externals.push({
+        "utf-8-validate": "commonjs utf-8-validate",
+        bufferutil: "commonjs bufferutil"
+      });
+  
+      return config;
+    },
+    images: {
+      domains: [
+        "uploadthing.com",
+        "utfs.io"
+      ]
+    }
+  }
+  
+  module.exports = nextConfig
